d0d65d7641170e929d228ef5530beccd944727ed
egomeh <jensgholm15@gmail.com>
2021-12-11 01:40:06 +0100

    hook now calls our code

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -7,6 +7,11 @@
 
 HMODULE self;
 
+__declspec(naked) void helm_manager_fixed_update_hook()
+{
+    self = self;
+}
+
 void Entry()
 {
     ON_EXIT
@@ -36,6 +41,31 @@ void Entry()
 
     if (!helm_manager_fixed_update)
         return;
+
+    void* fixed_update_hook_point = (void*)((size_t)helm_manager_fixed_update + 0x43);
+
+    DWORD old_protection;
+    VirtualProtect(fixed_update_hook_point, 4096, PAGE_EXECUTE_READWRITE, &old_protection);
+
+    uint32_t target_address = (uint32_t)&helm_manager_fixed_update_hook;
+    unsigned char fixed_update_hook[] =
+    {
+        0x68,                                       // push 4-byte imm
+        (unsigned char)(target_address >>  0),      // target
+        (unsigned char)(target_address >>  8),      // target
+        (unsigned char)(target_address >> 16),      // target
+        (unsigned char)(target_address >> 24),      // target
+        0xc3,                                       // ret
+        0x90,                                       // nop
+    };
+
+    memcpy(fixed_update_hook_point, fixed_update_hook, sizeof(fixed_update_hook));
+
+    DWORD dummy_protection;
+    VirtualProtect(fixed_update_hook_point, 4096, old_protection, &dummy_protection);
+
+    while (true)
+        Sleep(500);
 
     return;
 }
