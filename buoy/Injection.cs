819d0f3dc41c85ae4c7a24f90cab00108495f40b
egomeh <jensgholm15@gmail.com>
2021-12-19 19:05:15 +0100

    Restructure and add buoy controller C# program

---
+++ buoy/Injection.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+class Injection
+{
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+    public static bool InjectDLL(string ProcessName, string DLLPath)
+    {
+        Process[] processes = Process.GetProcessesByName("Cold*");
+
+        return true;
+    }
+}
---
+++ buoy/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+class Buoy
+{
+    public static int Main()
+    {
+        Injection.InjectDLL("ColdWaters.exe", "something.dll");
+
+        return 0;
+    }
+}
