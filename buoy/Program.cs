768817d4c4bc4ef7dd8ef1b10ff88c2dee1392df
egomeh <jensgholm15@gmail.com>
2021-12-22 23:41:37 +0100

    working on voice commands

--- buoy/Program.cs
+++ buoy/Program.cs
@@ -1,12 +1,33 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Speech.Recognition;
 
 class Buoy
 {
+    private bool quit;
+
     public static int Main()
+    {
+        Buoy buoy = new Buoy();
+        return buoy.Run();
+    }
+
+    int Run()
     {
         string sonarDLLPath = Path.GetFullPath(@"../../../../Debug/sonar.dll");
 
-        Injection.InjectDLL("ColdWaters", sonarDLLPath);
+        // Injection.InjectDLL("ColdWaters", sonarDLLPath);
+
+        Voice voice = new Voice();
+        voice.StartListeningForCommands();
+
+        quit = false;
+        while (!quit)
+        {
+            string? command = null;
+            voice.commandQueue.TryDequeue(out command);
+
+            if (!string.IsNullOrEmpty(command))
+                Console.WriteLine(String.Format("Voice command: {0}", command));
+        }
 
         return 0;
     }
---
+++ buoy/Voice.cs
@@ -0,0 +1,69 @@
+﻿using System.Speech.Recognition;
+using System.Collections.Concurrent;
+
+class Voice
+{
+    public ConcurrentQueue<string> commandQueue;
+
+    public Voice()
+    {
+        commandQueue = new ConcurrentQueue<String>();
+    }
+
+    public void StartListeningForCommands()
+    {
+        SpeechRecognizer sr = new SpeechRecognizer();
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+
+        List<GrammarBuilder> commands = new List<GrammarBuilder>();
+
+        List<string> numbers = new List<string>();
+
+        for (int i = 0; i <= 1000; ++i)
+        {
+            numbers.Add(String.Format("{0}", i));
+        }
+
+        Choices zeroToThousand = new Choices(numbers.ToArray());
+
+        // dive at/make depth
+        Choices divePrefix = new Choices(new String[] { "make depth", "dive at" });
+        GrammarBuilder depthCommandGrammarBuilder = new GrammarBuilder();
+        depthCommandGrammarBuilder.Append(divePrefix);
+        depthCommandGrammarBuilder.Append(zeroToThousand);
+        depthCommandGrammarBuilder.Append("feet");
+        commands.Add(depthCommandGrammarBuilder);
+
+        // set speed
+        Choices speedOptions = new Choices(new String[] { "slow", "half", "full", "flank" });
+        GrammarBuilder speedCommandBuilder = new GrammarBuilder();
+        speedCommandBuilder.Append(speedOptions);
+        speedCommandBuilder.Append("speed");
+        commands.Add(speedCommandBuilder);
+
+        // full rudder
+        Choices directionOption = new Choices(new String[] { "left", "right" });
+        GrammarBuilder fullRudderBuilder = new GrammarBuilder();
+        fullRudderBuilder.Append("full");
+        fullRudderBuilder.Append(directionOption);
+        fullRudderBuilder.Append("rudder");
+        commands.Add(fullRudderBuilder);
+
+        commands.Add(new GrammarBuilder("neutral rudder"));
+
+        commands.Add(new GrammarBuilder("launch decoy"));
+
+        Choices allChoices = new Choices(commands.ToArray());
+        GrammarBuilder finalCommand = new GrammarBuilder(allChoices);
+        Grammar grammar = new Grammar(finalCommand);
+        sr.LoadGrammar(grammar);
+
+        // sr.SpeechRecognized += new EventHandler(sr_SpeechRecognized);
+        sr.SpeechRecognized += sr_SpeechRecognized;
+    }
+
+    void sr_SpeechRecognized(object? sender, SpeechRecognizedEventArgs e)
+    {
+        commandQueue.Enqueue(e.Result.Text);
+    }
+}
